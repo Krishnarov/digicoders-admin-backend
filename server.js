4d2ef08bc371a8375d2a06d1cdc2a6e4c4bc31f1
Krishna kumar <krishnakumar@Krishnas-MacBook-Air.local>
2025-07-24 19:32:23 +0530

    seciond commit

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ const limiter = rateLimit({
 // CORS configuration
 app.use(cors({
   // origin: process.env.CORS_ORIGIN,
-  origin: ["http://localhost:5173","http://localhost:5174"],
+  origin: ["http://localhost:5173","http://localhost:5174","https://thedigicodersadmin.netlify.app"],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS','PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization']
